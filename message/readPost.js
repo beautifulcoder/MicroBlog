ce2af3e82f869f336ef698fca03c1f6d1446d0c3
Camilo Reyes <reyesc@gmail.com>
2016-10-22 20:12:56 -0500

    Add view to posts

--- message/readPost.js
+++ message/readPost.js
@@ -1,12 +1,13 @@
 var fs = require('fs');
 var path = require('path');
+var marked = require('marked');
 
 function readPost(postPath, postRender) {
   var fullPath = path.join(__dirname, '../') + postPath + '.md';
 
   fs.readFile(fullPath, 'utf-8', function postContent(err, text) {
     var content = {
-      postContent: text
+      postContent: marked(text)
     };
 
     postRender(err, content);
