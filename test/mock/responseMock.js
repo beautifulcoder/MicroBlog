78ea8278ac65eda45fafcb8b8fdef95e3b1071b5
Camilo Reyes <reyesc@gmail.com>
2016-12-31 09:31:07 -0600

    Reduce response mock

--- test/mock/responseMock.js
+++ test/mock/responseMock.js
@@ -2,14 +2,8 @@ var Response = function Response() {
   this.result = '';
 };
 
-Response.prototype.writeHead = function writeHead(returnCode, headers) {
+Response.prototype.writeHead = function writeHead(returnCode) {
   this.result += returnCode + ';';
-
-  for (var prop in headers) {
-    if (headers.hasOwnProperty(prop)) {
-      this.result += headers[prop] + ';';
-    }
-  }
 };
 
 Response.prototype.end = function end(body) {
